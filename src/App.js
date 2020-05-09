8cc27bb184ec9ad428725fcfbd1754615588c907
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-09 19:20:13 +0200

    Added a paragraph

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React app!</h1>
+        <h1>Hi, I'm a React app!!!</h1>
+        <p>This is really working§</p>
       </div>
     );
     // return React.createElement(
