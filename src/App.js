da724fd6f271db93956a2135b24e2cac457d43a5
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-09 22:26:52 +0200

    Passed a method reference between components

--- src/App.js
+++ src/App.js
@@ -31,17 +31,20 @@ class App extends Component {
         <button onClick={this.changeStateHandler}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
-          age={this.state.persons[0].age}>
+          age={this.state.persons[0].age}
+          click={this.changeStateHandler}>
           My Hobby: Volley-Ball
         </Person>
         <Person
           name={this.state.persons[1].name}
-          age={this.state.persons[1].age}>
+          age={this.state.persons[1].age}
+          click={this.changeStateHandler}>
           My Hobby: Racing
         </Person>
         <Person
           name={this.state.persons[2].name}
-          age={this.state.persons[2].age}>
+          age={this.state.persons[2].age}
+          click={this.changeStateHandler}>
           My Hobby: Dancing
         </Person>
       </div>
