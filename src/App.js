e65d223b45f41605fab389228d6bb6d2412b5610
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-09 21:27:48 +0200

    Handled onClick event

--- src/App.js
+++ src/App.js
@@ -11,11 +11,16 @@ class App extends Component {
     ]
   };
 
+  switchNameHandler = () => {
+    console.log("Clicked!")
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app!!!</h1>
         <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
           My Hobbies: Racing
