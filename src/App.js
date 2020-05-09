fd5de06092ef55a2d7dac3b34dec6a9e7a9d6521
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-09 16:19:53 +0200

    Fixed indentation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hi, I'm a React app!</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
